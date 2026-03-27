2877ee72b99ef53e980a7cd8a328c75899ee24f0
Possiblyai <imrana50280@gmail.com>
2026-03-27 17:15:22 -0400

    Added postgres db connection via the pqx package

---
+++ backend/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var DbConn *pgx.Conn
+
+func get_db_connection_string() string {
+	db_user := os.Getenv("DB_USER")
+	db_pass := os.Getenv("DB_PASS")
+	db_addr := os.Getenv("DB_ADDR")
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", db_user, db_pass, db_addr, db_port, db_name)
+}
+
+func init() {
+	dbConn, err := pgx.Connect(context.Background(), get_db_connection_string())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if err := dbConn.Close(context.Background()); err != nil {
+			log.Fatalf("Error in closing database: %s", err)
+		}
+	}()
+}
