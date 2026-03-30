b74824ae75a332b44ad5b51e98c8f99cfa46f47e
LoadinConfusion <schaarlea@gmail.com>
2026-03-30 19:52:23 -0400

    Home Page Update
    The styling was updated, as well as the logo added, and overall, following the Figma design for the website.

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,12 +1,17 @@
 <template>
   <main>
-    <body>
-      <h1>Getting Started</h1>
-      <p>Welcome to Banana Waffle Cookies.</p>
+    <div class="home-content">
+      <div class="title-row">
+        <RouterLink to="/" class="title-logo-link" aria-label="Go to home page">
+          <img class="title-logo" src="/images/bwc.png" alt="BWC Logo">
+        </RouterLink>
+        <h1>Banana Waffle<br>Cookies</h1>
+      </div>
+      <p>Keep applications organized, generate tailored resumes, and manage statuses in one place!</p>
       <div>
         <RouterLink to="/login" class="button">Get Started</RouterLink>
       </div>
-    </body>
+    </div>
   </main>
 </template>
 
