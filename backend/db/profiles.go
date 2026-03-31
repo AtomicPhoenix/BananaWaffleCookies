70427d34b10fbdf1d614618f0198ebf5fcc34445
Possiblyai <imrana50280@gmail.com>
2026-03-31 17:30:37 -0400

    Updated Users struct to export its fields. Updated functions accordingly. Also made it so you can create a profile with only a uid

--- backend/db/profiles.go
+++ backend/db/profiles.go
@@ -20,11 +20,8 @@ type Profile struct {
 	completion    int
 }
 
-func createProfile(profile Profile) bool {
-	var sql string = `INSERT INTO 
-				profiles (user_id, first_name, last_name, phone, city, state, country, linkedin_url, portfolio_url, summary, completion_percent) 
-				VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
-	err := DbConn.QueryRow(context.Background(), sql, profile.user_id, profile.first_name, profile.last_name, profile.phone, profile.city, profile.state, profile.country, profile.linkedin_url, profile.portfolio_url, profile.summary, profile.completion)
+func createProfile(uid int) bool {
+	err := DbConn.QueryRow(context.Background(), "INSERT INTO profiles (user_id) VALUES ($1)", uid)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create profile: %v\n", err)
 		return false
--- backend/db/users.go
+++ backend/db/users.go
@@ -7,31 +7,36 @@ import (
 )
 
 type User struct {
-	email         string
-	password_hash string
+	Id            int
+	Email         string
+	Password_hash string
 }
 
-func GetUserID(email string) int {
-	var id int
-	err := DbConn.QueryRow(context.Background(), "SELECT id FROM users WHERE email=$1", email).Scan(&id)
+func GetUser(email string) (User, error) {
+	var user User
+	err := DbConn.QueryRow(context.Background(), "SELECT id, email, password_hash FROM users WHERE email=$1", email).Scan(&user.Id, &user.Email, &user.Password_hash)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to get user: %v\n", err)
+		return user, err
 	}
-	return id
+	return user, nil
 }
 
-func RegisterUser(user User, profile Profile) bool {
-	err := DbConn.QueryRow(context.Background(), "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id", user.email, user.password_hash).Scan(&profile.user_id)
+func RegisterUser(user User) (int, error) {
+	var uid int
+	fmt.Printf("Registering user %v\n", user)
+	err := DbConn.QueryRow(context.Background(), "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id", user.Email, user.Password_hash).Scan(&uid)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to insert user into database: %v\n", err)
-		return false
+		return uid, err
 	}
-	return createProfile(profile)
+	createProfile(uid)
+	return uid, nil
 }
 
-func LoginUser(email string, password_hash string) (bool, int) {
+func LoginUser(user User) (bool, int) {
 	var uid int = -1
-	err := DbConn.QueryRow(context.Background(), "SELECT uid FROM users WHERE email=$1 AND password_hash=$2", email, password_hash).Scan(&uid)
+	err := DbConn.QueryRow(context.Background(), "SELECT uid FROM users WHERE email=$1 AND password_hash=$2", user.Email, user.Password_hash).Scan(&uid)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to authenticate user: %v\n", err)
 		return false, -1
