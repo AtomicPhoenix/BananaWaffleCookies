2e061981f70213d867a497f36415612907df8481
Possiblyai <imrana50280@gmail.com>
2026-03-31 19:55:18 -0400

    Backend code for updating profiles

--- backend/db/profiles.go
+++ backend/db/profiles.go
@@ -28,3 +28,15 @@ func createProfile(uid int) bool {
 	}
 	return true
 }
+
+func UpdateProfile(profile Profile) error {
+	var sql string = `UPDATE profiles
+				SET first_name = $1, last_name = $2, phone = $3, city = $4, state = $5, country = $6, linkedin_url = $7, portfolio_url = $8, summary = $9, completion_percent = $10
+				WHERE user_id = $11`
+	_, err := DbConn.Exec(context.Background(), sql, profile.first_name, profile.last_name, profile.phone, profile.city, profile.state, profile.country, profile.linkedin_url, profile.portfolio_url, profile.summary, profile.completion_percent, profile.user_id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to insert user into database: %v\n", err)
+		return err
+	}
+	return nil
+}
