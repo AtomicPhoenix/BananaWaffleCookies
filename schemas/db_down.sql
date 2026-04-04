52aad654b4cf20bda42b05f94adcb1690f666104
Frank Crawford <fjcrawford1@gmail.com>
2026-04-04 15:52:57 -0400

    adding experiences and profile preferences

--- schemas/db_down.sql
+++ schemas/db_down.sql
@@ -2,11 +2,17 @@
 -- DOWN SCRIPT
 -- =========================
 
--- Drop child tables first
+-- Drop child tables first (reverse dependency order)
+
 DROP TABLE IF EXISTS job_document_links;
 DROP TABLE IF EXISTS document_versions;
 DROP TABLE IF EXISTS documents;
 DROP TABLE IF EXISTS job_activities;
 DROP TABLE IF EXISTS jobs;
+
+DROP TABLE IF EXISTS profile_skills;
+DROP TABLE IF EXISTS profile_education;
+DROP TABLE IF EXISTS profile_experiences;
+
 DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS users;
