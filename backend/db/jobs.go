f4eaa97cb256de4b232606b97a4f0d0a47f59578
Frank Crawford <fjcrawford1@gmail.com>
2026-04-07 19:17:53 -0400

    changes to references in backend

--- backend/db/jobs.go
+++ backend/db/jobs.go
@@ -11,7 +11,8 @@ type Job struct {
 	user_id       int
 	company_name  string
 	title         string
-	location_text string
+	city          string
+	state  		  string
 	posting_url   string
 	status        string // in ('interested', 'applied', 'interview', 'offer', 'rejected', 'archived')
 	deadline_date time.Time
@@ -37,10 +38,10 @@ func GetJob(title string) int {
 
 func CreateJob(job Job) (bool, int) {
 	var id int
-	sql := `INSERT INTO jobs (user_id, company_name, title, location_text, posting_url, status, deadline_date, notes) 
+	sql := `INSERT INTO jobs (user_id, company_name, title, city, state, posting_url, status, deadline_date, notes) 
 				VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
 				RETURNING id`
-	err := DbConn.QueryRow(context.Background(), sql, job.user_id, job.company_name, job.title, job.location_text, job.posting_url, job.status, job.deadline_date, job.notes).Scan(&id)
+	err := DbConn.QueryRow(context.Background(), sql, job.user_id, job.company_name, job.title, job.city, job.state, job.posting_url, job.status, job.deadline_date, job.notes).Scan(&id)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to insert job into database: %v\n", err)
 		return false, -1
