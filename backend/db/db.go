1a22491a65cf02cac0e81b0d740b801256c6caa3
Possiblyai <imrana50280@gmail.com>
2026-04-07 21:44:56 -0400

    Moved db testing logic out of db.go. Moved initialization into main() function

--- backend/db/db.go
+++ backend/db/db.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 )
 
 var DbConn *pgxpool.Pool
@@ -17,48 +16,29 @@ func get_db_connection_string() string {
 	db_addr := os.Getenv("DB_ADDR")
 	db_port := os.Getenv("DB_PORT")
 	db_name := os.Getenv("DB_NAME")
+
+	if db_name == "" {
+		panic("DB_NAME is not set")
+	}
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", db_user, db_pass, db_addr, db_port, db_name)
 }
 
-func init() {
-	fmt.Printf("-----------------------\nInitializing Database\n")
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to load env file: %v\n", err)
-		os.Exit(1)
-	}
-
+func initDB() error {
+	fmt.Println("Initializing Database")
+	var err error
 	DbConn, err = pgxpool.New(context.Background(), get_db_connection_string())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		return err
 	}
-
-	if err = DbConn.Ping(context.Background()); err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to ping database: %v\n", err)
-		os.Exit(1)
-	}
-
 	fmt.Println("Successfully connected to database.")
-
-	checkTables()
-
-	fmt.Printf("Database setup completed!\n-----------------------\n")
+	return nil
 }
 
 func main() {
-	defer DbConn.Close()
-}
-
-func checkTables() {
-	var result bool
-	tables := []string{"users", "profiles", "jobs", "job_activities", "documents", "document_versions"}
-	for _, table := range tables {
-		err := DbConn.QueryRow(context.Background(), "SELECT EXISTS ( SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = $1);", table).Scan(&result)
-		if err != nil || result == false {
-			fmt.Fprintf(os.Stderr, "Table %s does not exist: %v\n", table, err)
-			os.Exit(1)
-		}
+	err := initDB()
+	if err != nil {
+		os.Exit(1)
 	}
-	fmt.Println("All tables exist!")
+	defer DbConn.Close()
 }
