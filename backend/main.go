005911f3a22093dc93e622628a92fdb9942c7312
Possiblyai <imrana50280@gmail.com>
2026-04-13 16:28:45 -0400

    Added job deletion api route

--- backend/main.go
+++ backend/main.go
@@ -66,6 +66,7 @@ func main() {
 		r.Get("/api/profile", handlers.GetProfile)
 		r.Get("/api/jobs", handlers.GetJobs)
 		r.Get("/api/jobs/{id}", handlers.GetJob)
+		r.Delete("/api/jobs/{id}", handlers.DeleteJob)
 		r.Post("/api/jobs", handlers.CreateJob)
 		r.Put("/api/jobs", handlers.UpdateJob)
 		r.Put("/api/settings", handlers.UpdateSettings)
