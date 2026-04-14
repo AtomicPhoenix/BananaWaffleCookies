bff4a7e1787bc0c08fe78d39a88b9c9da1b23b57
Possiblyai <imrana50280@gmail.com>
2026-04-14 19:01:47 -0400

    Fixed broken IsJobOwner function

--- backend/db/jobs.go
+++ backend/db/jobs.go
@@ -316,26 +316,26 @@ func GetJobActivities(jobID int) ([]JobActivity, error) {
 
 func IsJobOwner(jobID int, userID int) (bool, error) {
 	query := `
-		SELECT is_owner (
+		SELECT EXISTS (
 			SELECT 1
 			FROM jobs
 			WHERE id = $1 AND user_id = $2
 		);
 	`
 
-	var is_owner bool
+	var exists bool
 
 	err := DbConn.QueryRow(
 		context.Background(),
 		query,
 		jobID,
 		userID,
-	).Scan(&is_owner)
+	).Scan(&exists)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to verify job ownership: %v\n", err)
 		return false, err
 	}
 
-	return is_owner, nil
+	return exists, nil
 }
