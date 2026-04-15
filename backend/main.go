5292464c4cf06da7df53858032684b55a954e5e3
Possiblyai <imrana50280@gmail.com>
2026-04-14 20:05:09 -0400

    Added backend db/handler functions to linking documents to jobs

--- backend/main.go
+++ backend/main.go
@@ -110,6 +110,11 @@ func main() {
 					r.Put("/{followup_id}", handlers.UpdateFollowUp)
 					r.Delete("/{followup_id}", handlers.DeleteFollowUp)
 				})
+				r.Route("/documents", func(r chi.Router) {
+					r.Get("/", handlers.GetJobDocuments)
+					r.Post("/", handlers.LinkDocumentToJob)
+					r.Delete("/{document_id}", handlers.UnlinkDocumentFromJob)
+				})
 			})
 
 		})
